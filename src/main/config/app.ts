63b16be1ac6eef7df3ac19b0231110e5be9a7e15
Gabriel Doyle Balk <gdoyle.dev@gmail.com>
2021-04-25 16:10:54 -0300

    chore: add setupAppoloServer main

--- src/main/config/app.ts
+++ src/main/config/app.ts
@@ -1,9 +1,10 @@
 import { setupRoutes } from './routes'
-
+import { setupApolloServer } from './apollo-server'
 import express from 'express'
 
 const app = express()
 
 setupRoutes(app)
+setupApolloServer(app)
 
 export default app
