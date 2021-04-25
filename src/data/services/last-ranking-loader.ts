4eab6a2f67b777f92144f814d77a05076cf50c69
Gabriel Doyle Balk <gdoyle.dev@gmail.com>
2021-04-25 00:51:43 -0300

    chore: add usecase logic

---
+++ src/data/services/last-ranking-loader.ts
@@ -0,0 +1,14 @@
+import { RankingScore } from '../../domain/entities'
+import { RankingUnavailableError } from '../../domain/errors'
+import { LastRankingLoader } from '../../domain/usecases'
+import { LoadLastRankingRepository } from '../contracts'
+
+export class LastRankingLoaderService implements LastRankingLoader {
+  constructor (private readonly loadLastRankingRepository: LoadLastRankingRepository) { }
+  async load (): Promise<RankingScore[]> {
+    if (new Date().getHours() > 21) {
+      throw new RankingUnavailableError()
+    }
+    return await this.loadLastRankingRepository.loadLastRanking()
+  }
+}
