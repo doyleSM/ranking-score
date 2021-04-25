5e02a54d01f043a864ab82ad356482c2571b6667
Gabriel Doyle Balk <gdoyle.dev@gmail.com>
2021-04-25 16:12:50 -0300

    refactor: fix typedef graphql

--- src/main/graphql/type-defs/ranking.ts
+++ src/main/graphql/type-defs/ranking.ts
@@ -4,19 +4,17 @@ export default gql`
   extend type Query {
     lastRanking: [RankingScore!]
   }
-  type RankingScore = {
+  type RankingScore {
     player: Player!
     score: Int!
     matchDate: String!
     heroes: [Hero!]!
   }
-
-  type Player = {
+  type Player {
     name: String!
     country: String!
   }
-
-  type Hero = {
+  type Hero {
     name: String!
     level: Int!
   }
